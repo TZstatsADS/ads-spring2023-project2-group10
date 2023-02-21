f31b2cf8dd53aaddf7ccd03a3edb214de5fd0d5b
jh4578 <jh4578@columbia.edu>
2023-02-21 10:46:21 -0500

    shinny app

--- app/ui.R
+++ app/ui.R
@@ -1,74 +1,190 @@
 
-if (!require("shiny")) {
-  install.packages("shiny")
-  library(shiny)
-}
-if (!require("shinyWidgets")) {
-  install.packages("shinyWidgets")
-  library(shinyWidgets)
-}
-if (!require("shinythemes")) {
-  install.packages("shinythemes")
-  library(shinythemes)
-}
-if (!require("leaflet")) {
-  install.packages("leaflet")
-  library(leaflet)
-}
-if (!require("leaflet.extras")) {
-  install.packages("leaflet.extras")
-  library(leaflet.extras)
-}
-
-# Define UI for application that draws a histogram
-shinyUI(
-    navbarPage(strong("Citi Bike Study",style="color: white;"), 
-               theme=shinytheme("cerulean"), # select your themes https://rstudio.github.io/shinythemes/
-#------------------------------- tab panel - Maps ---------------------------------
-                tabPanel("Maps",
-                         icon = icon("map-marker-alt"), #choose the icon for
-                         div(class = 'outer',
-                        # side by side plots
-                        fluidRow(
-                                splitLayout(cellWidths = c("50%", "50%"), 
-                                             leafletOutput("left_map",width="100%",height=1200),
-                                             leafletOutput("right_map",width="100%",height=1200))),
-                        #control panel on the left
-                        absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
-                                      top = 200, left = 50, right = "auto", bottom = "auto", width = 250, height = "auto",
-                                      tags$h4('Citi Bike Activity Comparison'), 
-                                      tags$br(),
-                                      tags$h5('Pre-covid(Left) Right(Right)'), 
-                                      prettyRadioButtons(
-                                                      inputId = "adjust_score",
-                                                      label = "Score List:", 
-                                                      choices = c("start_cnt", 
-                                                                  "end_cnt", 
-                                                                  "day_diff_absolute",
-                                                                  "day_diff_percentage"),
-                                                      inline = TRUE, 
-                                                      status = "danger",
-                                                      fill = TRUE
-                                                        ),
-                                      awesomeRadio("adjust_time", 
-                                                   label="Time",
-                                                    choices =c("Overall",
-                                                               "Weekday", 
-                                                               "Weekend"), 
-                                                    selected = "Overall",
-                                                    status = "warning"),
-                                      # selectInput('adjust_weather',
-                                      #             label = 'Adjust for Weather',
-                                      #             choices = c('Yes','No'), 
-                                      #             selected = 'Yes'
-                                      #             ),
-                                      style = "opacity: 0.80"
-                                      
-                                ), #Panel Control - Closing
-                            ) #Maps - Div closing
-                        ) #tabPanel maps closing
-   
+library(viridis)
+library(dplyr)
+library(tibble)
+library(tidyverse)
+library(shinythemes)
+library(sf)
+library(RCurl)
+library(tmap)
+library(rgdal)
+library(leaflet)
+library(shiny)
+library(shinythemes)
+library(plotly)
+library(ggplot2)
+library(shinydashboard)
 
 
-    ) #navbarPage closing  
-) #Shiny UI closing    
+# Define UI ----
+body <- dashboardBody(
+    
+    tabItems(
+        # ------------------ Home ----------------------------------------------------------------
+        
+        tabItem(tabName = "Home", fluidPage(
+            fluidRow(box(width = 15, title = "Introduction", status = "primary",
+                         solidHeader = TRUE, h3("Covid-19 and NYC business"),
+                         h4("By Wendy Doan, Qizhen Yang, Qiao Li, Yandong Xiong, James Bergin Smiley"),
+                         h5("Drawing data from multiple sources, this application provides insight into the economic impact of coronavirus 2019 (COVID-19) on New York’s city economy. The results shed light on both the financial fragility of many businesses, and the significant impact COVID-19 had on these businesses in the weeks after the COVID-19–related disruptions began."),
+                         h5("The application will mainly track down the change in the number of businesses being closed or newly opened across Covid timeline. We divided the businesses into 4 types:", strong("Retail, Service, Food and Beverage, Entertainment")))),
+            fluidRow(box(width = 15, title = "Targeted User", status = "primary", solidHeader=TRUE,
+                         h5("We believe that the application would be useful for anyone who are interested in learning more about the effects of Covid 19"))),
+            fluidRow(box(width = 15, title = "How to Use The App", status = "primary",
+                         solidHeader = TRUE,
+                         h5("The application is divided into 5 separate tabs"),
+                         tags$div(tags$ul(
+                             tags$li("The", strong("first"), "tab: Introduction"),
+                             tags$li("The", strong("second"), "tab: The detailed ZIP code map shows the extent of Covid 19 outbreak in NYC. It provided key information including: confirmed cases, infection rate, number of business that are closed in the neighborhood"),
+                             tags$li("The", strong("third and fourth"), "tab: stats on recently opened/ closed business during Covid 19, tracked separately for different industries"),
+                             tags$li("The", strong("fifth"),"tab: Appendix and data sources")
+                             
+                         ))
+            ))
+        )), # end of home 
+        # ------------------ Map-----------------------------------
+        tabItem(tabName = "Map",
+                h2("?", align = 'center'),
+                leafletOutput("nyc_map_covid", width = "100%", height = 800)
+        ),
+        
+        #------------------Covid 19 Health Issues----------------------------
+        tabItem(tabName = "Health_Issue", fluidPage(
+            
+            # App title ----
+            titlePanel("Health Issue"),
+            
+            # Sidebar layout with input and output definitions ----
+            sidebarLayout(
+                
+                # Sidebar panel for inputs ----
+                sidebarPanel(
+                    
+                  # Input: Select for the borough ----
+                  selectInput(inputId = "borough",
+                              label = "Choose a borough:",
+                              choices = c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island", "All")),
+                  selectInput(inputId = "weight",
+                              label = "Whether the data is weighted by the population:",
+                              choices = c("Unweighted", "Weighted"))
+                   
+                ),
+                
+                # Main panel for displaying outputs ----
+                mainPanel(
+                    
+                  # Output: Plot on Covid case count ----
+                  plotOutput(outputId = "case_plot"),
+                  
+                  # Output: Plot on hospitalization count ----
+                  plotOutput(outputId = "hosp_plot"),
+                  
+                  # Output: Plot on death count ----
+                  plotOutput(outputId = "death_plot"),
+                  
+                  # Output: Barplot on 2020 cumulative counts
+                  plotOutput(outputId = "barplot_2020"),
+                  
+                  # Output: Barplot on 2021 cumulative counts
+                  plotOutput(outputId = "barplot_2021"),
+                  
+                  # Output: Barplot on 2022 cumulative counts
+                  plotOutput(outputId = "barplot_2022")
+                    
+                )
+            )
+        )
+        ), 
+
+        #------------------Salary and Worktime----------------------------
+        tabItem(tabName = "Salary_Worktime", fluidPage(
+            
+            # App title ----
+            titlePanel("Salary and Worktime"),
+            
+            # Sidebar layout with input and output definitions ----
+            sidebarLayout(
+                
+                # Sidebar panel for inputs ----
+                sidebarPanel(
+                    # Input: Select for the borough ----
+                    
+                    
+                    # Input: Select for the borough ----
+                    selectInput(inputId = "Borough",
+                                label = "Choose a borough:",
+                                choices = c("Manhattan", "Bronx", "Brooklyn", "Queens"))
+                    
+                ),
+                
+                # Main panel for displaying outputs ----
+                mainPanel(
+                    
+                )
+            )
+        )
+        ),
+        
+
+
+        
+        # ------------------ Appendix --------------------------------
+        tabItem(tabName = "Appendix", fluidPage( 
+            HTML(
+                "<h2> Data Sources </h2>
+                <h4> <p><li>NYC Covid 19 Data: <a href='https://github.com/nychealth/coronavirus-data'>NYC covid 19 github database</a></li></h4>
+                <h4><li>NYC COVID-19 Policy : <a href='https://www1.nyc.gov/site/coronavirus/businesses/businesses-and-nonprofits.page' target='_blank'> NYC Citywide Information Portal</a></li></h4>
+                <h4><li>NYC Business data : <a href='https://data.cityofnewyork.us/Business/Legally-Operating-Businesses/w7w3-xahh' target='_blank'>NYC Open Data</a></li></h4>
+                <h4><li>NYC Business Application Data : <a href='https://data.cityofnewyork.us/Business/License-Applications/ptev-4hud' target='_blank'>NYC Open Data</a></li></h4>
+                <h4><li>NYC Minority Owned Business : <a href='https://data.cityofnewyork.us/Business/M-WBE-LBE-and-EBE-Certified-Business-List/ci93-uc8s' target='_blank'>NYC Health + Hospitals</a></li></h4>
+                <h4><li>NYC Geo Data : <a href='https://github.com/ResidentMario/geoplot-data-old' target='_blank'> Geoplot Github</a></li></h4>"
+            ),
+            
+            titlePanel("Disclaimers "),
+            
+            HTML(
+                " <p>We drew our business insights from NYC Open data, specifically business expiration databases. We recognized that there would be a lag between when the business is closed and when the expiration date, status are updated.</p>",
+                " <p>Thus our app may understate the number of businesses that were actually closed. Furthermore, due to the lag between the time point where the business were closed, with when the expiration date be updated, there could be some uncertainty to define on which day, month the businesses were fully closed
+ </p>"),
+            
+            titlePanel("Acknowledgement  "),
+            
+            HTML(
+                " <p>This application is built using R shiny app.</p>",
+                "<p>The following R packages were used in to build this RShiny application:</p>
+                <li>Shinytheme</li>
+                <li>Tidyverse</li>
+                <li>Dyplr</li><li>Tibble</li><li>Rcurl</li><li>Plotly</li>
+                <li>ggplot2</li>"
+            ),
+            
+            titlePanel("Contacts"),
+            
+            HTML(
+                " <p>For more information please feel free to contact</p>",
+                " <p>Wendy Doan(ad3801@columbia.edu) </p>",
+                " <p>Qizhen Yang(qy2446@columbia.edu)</p>",
+                " <p>Yandong Xiong(yx2659@columbia.edu) </p>",
+                " <p>TQiao Li(ql2403@columbia.edu)</p>",
+                " <p>James Smiley(jbs2253@columbia.edu) </p>")
+        )) # end of tab
+        
+    ) # end of tab items
+) # end of body
+
+
+
+ui <- dashboardPage(
+    title="Covid 19 and The effect on Business in NYC",
+    skin = "blue", 
+    dashboardHeader(title=span("Covid 19 and NYC Business",style="font-size: 16px")),
+    
+    dashboardSidebar(sidebarMenu(
+        menuItem("Home", tabName = "Home", icon = icon("home")),
+        menuItem("Map", tabName = "Map", icon = icon("compass")),
+        menuItem("Health Issue", tabName = "Health_Issue", icon = icon("dollar-sign")),
+        menuItem("Salary and Worktime", tabName = "Salary_Worktime", icon = icon("dollar-sign")),
+        menuItem("Appendix", tabName = "Appendix", icon = icon("fas fa-asterisk"))
+    )),
+    body 
+)
